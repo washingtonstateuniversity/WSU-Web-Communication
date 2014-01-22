888b6bdced55949d34d74703b8f7284264e9fe4a
jeremyfelt <jeremy.felt@gmail.com>
2014-01-22 10:12:34 -0800

    Allow for a local-config and prod-config

---
+++ web/wp-config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Configure WordPress
+ */
+
+if ( '10.10.40.40' === $_SERVER['SERVER_ADDR'] ) {
+	include( dirname( __FILE__ ) . '/local-config.php' );
+	define( 'WP_LOCAL_DEV', true );
+} else {
+	include( dirname( __FILE__ ) . '/prod-config.php' );
+	define( 'WP_LOCAL_DEV', false );
+}
+
+$table_prefix  = 'wp_';
+
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', ''     );
+define( 'WPLANG',     ''     );
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
